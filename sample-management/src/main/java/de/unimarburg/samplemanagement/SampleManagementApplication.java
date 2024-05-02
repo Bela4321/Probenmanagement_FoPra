daff2f0fd4bf2979929f7629ba2e218fcc46f5c9
Bela <bela.schinke@gmail.com>
2024-05-02 22:31:58 +0200

    initial Commit

---
+++ sample-management/src/main/java/de/unimarburg/samplemanagement/SampleManagementApplication.java
@@ -0,0 +1,13 @@
+package de.unimarburg.samplemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleManagementApplication.class, args);
+	}
+
+}
---
+++ sample-management/src/test/java/de/unimarburg/samplemanagement/SampleManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package de.unimarburg.samplemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
