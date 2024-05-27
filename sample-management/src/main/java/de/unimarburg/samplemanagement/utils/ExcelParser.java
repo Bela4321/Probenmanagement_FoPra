6b16c0271688890c1228c9885b279eceb6c1c867
meyerdav <meyerdav@students.uni-marburg.de>
2024-05-27 18:06:17 +0200

    Test Class added

--- sample-management/src/main/java/de/unimarburg/samplemanagement/utils/ExcelParser.java
+++ sample-management/src/main/java/de/unimarburg/samplemanagement/utils/ExcelParser.java
@@ -1,14 +1,22 @@
 package de.unimarburg.samplemanagement.utils;
 
+import de.unimarburg.samplemanagement.model.Sample;
 import de.unimarburg.samplemanagement.model.Study;
 import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.Iterator;
+import java.util.List;
 
 public class ExcelParser {
+
+    public enum cellType {
+        STRING, NUMERIC, DATE, BOOLEAN
+    }
 
     public static Study readExcelFile(FileInputStream inputStream) throws IOException {
 
@@ -17,36 +25,100 @@ public class ExcelParser {
         Iterator<Row> iterator = sheet.iterator();
 
         String studyName;
+        List<Sample> sampleList = new ArrayList<>();
 
         // Reading first line
         if (iterator.hasNext()) {
             Row currentRow = iterator.next();
-            studyName = currentRow.getCell(1).getStringCellValue();
+            studyName = (String) getCellValue(currentRow.getCell(1), cellType.STRING);
             if (iterator.hasNext()) {
                 // Skip the header
                 iterator.next();
             }
             else {
-                // Wrong File format
-                return null;
+                throw new IOException("Wrong file format");
             }
         }
         else {
-            // Wrong File format
-            return null;
+            throw new IOException("Wrong file format");
         }
 
         while (iterator.hasNext()) {
             Row currentRow = iterator.next();
-            String name = currentRow.getCell(0).getStringCellValue();
-            int age = (int) currentRow.getCell(1).getNumericCellValue();
-            Person person = new Person(name, age);
-            personList.add(person);
+
+            //Creating Sample Objects
+            Sample sample = new Sample();
+            String coordinates = (String) getCellValue(currentRow.getCell(0), cellType.STRING);
+            sample.setCoordinates(coordinates);
+
+            //TODO: ID vom subject ist auto-generated?
+            double idDouble = (Double) getCellValue(currentRow.getCell(1), cellType.NUMERIC);
+            int id = getNumericValue(idDouble);
+
+            double visitDouble = (Double) getCellValue(currentRow.getCell(2), cellType.NUMERIC);
+            int visit = getNumericValue(idDouble);
+            sample.setVisits(visit);
+
+            Date date = (Date) getCellValue(currentRow.getCell(3), cellType.DATE);
+            sample.setSampleDate(date);
+
+            String amount = (String) getCellValue(currentRow.getCell(4), cellType.STRING);
+            sample.setSample_amount(amount);
+
+            //TODO: Not in Sample Class
+            String sampleType = (String) getCellValue(currentRow.getCell(5), cellType.STRING);
+            sample.setCoordinates(coordinates);
+
+            String barcode = (String) getCellValue(currentRow.getCell(6), cellType.STRING);
+            sample.setSample_barcode(barcode);
+
+            sampleList.add(sample);
         }
 
         workbook.close();
         inputStream.close();
 
-        return null;
+        //TODO: Auto-generated ID -> NULL??   Date nicht in Tabelle (aktuelles Date nehmen?)
+        Study study = new Study(null, studyName, null, sampleList);
+
+        return study;
+    }
+
+    public static Object getCellValue(Cell cell, cellType expectedType) throws IOException {
+        if (cell == null) {
+            return null;
+        }
+        switch (cell.getCellType()) {
+            case STRING:
+                if (expectedType == cellType.STRING) {
+                    return cell.getStringCellValue();
+                }
+                break;
+            case NUMERIC:
+                if (expectedType == cellType.DATE && DateUtil.isCellDateFormatted(cell)) {
+                    return cell.getDateCellValue();
+                } else if (expectedType == cellType.NUMERIC) {
+                    return cell.getNumericCellValue();
+                }
+                break;
+            case BOOLEAN:
+                if (expectedType == cellType.BOOLEAN) {
+                    return cell.getBooleanCellValue();
+                }
+                break;
+            case BLANK:
+                return "";
+            default:
+                throw new IOException("Unexpected cell type: " + cell.getCellType());
+        }
+        // If cell type does not match expected type, throw an exception
+        throw new IOException("Expected cell type: " + expectedType + ", but got: " + cell.getCellType());
+    }
+
+    private static int getNumericValue(double value) throws IOException {
+        if (value == (int) value) {
+            return (int) value;
+        }
+        throw new IOException("Expected int value, given Double");
     }
 }
---
+++ sample-management/src/test/java/de/unimarburg/samplemanagement/ExcelTest.java
@@ -0,0 +1,4 @@
+package de.unimarburg.samplemanagement;
+
+public class ExcelTest {
+}
