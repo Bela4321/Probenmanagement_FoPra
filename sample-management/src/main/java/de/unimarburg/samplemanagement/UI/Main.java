95559104ff9873755d0c2c8220c143ab9a43c757
Bela <bela.schinke@gmail.com>
2024-06-18 11:28:18 +0200

    Home resets user state

--- sample-management/src/main/java/de/unimarburg/samplemanagement/UI/Main.java
+++ sample-management/src/main/java/de/unimarburg/samplemanagement/UI/Main.java
@@ -1,17 +1,20 @@
 package de.unimarburg.samplemanagement.UI;
 
 import com.vaadin.flow.component.Text;
-import com.vaadin.flow.component.UI;
-import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.Route;
+import de.unimarburg.samplemanagement.service.ClientStateService;
 import de.unimarburg.samplemanagement.utils.SIDEBAR_FACTORY;
+import org.springframework.beans.factory.annotation.Autowired;
 
 
 @Route("/")
 public class Main extends HorizontalLayout {
-    public Main() {
+
+    @Autowired
+    public Main(ClientStateService clientStateService) {
+        clientStateService.setUserState(null);
         add(SIDEBAR_FACTORY.getSidebar(null));
         VerticalLayout verticalLayout = new VerticalLayout();
         verticalLayout.add(new Text("Welcome to the Sample Management System\n"));
